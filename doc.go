11016d8d3c07251852ba01246a6ceabc29ee2849
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-05-01 11:18:13 -0700

    new: licensing

--- doc.go
+++ doc.go
@@ -9,20 +9,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package phoenix
-
-import (
-	"go.aporeto.io/elemental"
-	"go.aporeto.io/gaia"
-)
-
-// HookFunc is the type of a function to that can be used as a Hook.
-type HookFunc func(string, elemental.Operation, gaia.RemoteProcessorModeValue, elemental.Identifiable, []string) error
-
-// HooksRegistry represents a list of HookFunc.
-type HooksRegistry []HookFunc
-
-// NewHooksRegistry returns a HooksRegistry with the given list of HookFunc.
-func NewHooksRegistry(plugins ...HookFunc) HooksRegistry {
-	return append(HooksRegistry{}, plugins...)
-}
+// Package phoenix contains a base bahamut server that can
+// be used to implelement an Aporeto API Hook endpoint.
+package phoenix // import "go.aporeto.io/phoenix"
